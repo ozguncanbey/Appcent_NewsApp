be9ba4f9eb000c9800814a6432bb01270f3c715e
Özgün Can Beydili <ozguncanbeydili@hotmail.com>
2024-05-08 15:38:07 +0300

    Initial Commit

---
+++ Appcent_NewsApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Appcent_NewsApp
+//
+//  Created by Özgün Can Beydili on 8.05.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
